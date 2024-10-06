67d4a571eb150597ef188062b37c2d63502f5a4e
Abdurrahim Uysal <abdurrahimuysal@hotmail.com>
2024-10-06 17:07:00 +0000

    Lesson 5 + benchmark side project

---
+++ Lessons/Lesson5.cpp
@@ -0,0 +1,185 @@
+#include "Lesson5_personal_side_project.hpp"
+#include <format>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+void navigate(std::string::iterator beg, std::string::iterator end) {
+  std::cout << "Nonconst\n";
+}
+void navigate(std::string::const_iterator beg,
+              std::string::const_iterator end) {
+  std::cout << "Const\n";
+}
+
+// Universal reference can be used without std::forward
+// Below example is taking advantage of universal reference holding constness
+// details
+template <typename T> void processContainer(T &&coll) {
+  navigate(coll.begin(), coll.end());
+}
+
+template <typename T> void func(T &, T &) {};
+
+template <typename T> void insert1(std::vector<T> &vec, T &&element) {
+  vec.push_back(std::forward<T>(element));
+}
+
+template <typename T>
+void insert2(std::vector<std::remove_reference_t<T>> &vec, T &&element) {
+  vec.push_back(std::forward<T>(element));
+}
+
+class MyClass {};
+
+void foo(MyClass &) {}
+void foo(MyClass &&) {}
+// Assume const versions are also defined
+
+template <typename C, typename T> void Fill(C &con, const T &val) {
+  for (auto &elem : con) { // This fails when C is vector<bool>
+    // for(auto&& elem:con){ // This fixes the issue with container with proxy
+    // classes
+    elem = val;
+    // auto& elem = pos.operator*(); // This is how compiler sees it,
+    // Error : assigning rvalue to lvalue as  reference proxy rvalue is returned
+  }
+}
+
+template <typename T>
+decltype(auto)
+perfectReturnFunc(T &&t) { // Allow us to return with exact return type of foo
+  return foo(std::forward<T>(t));
+}
+
+MyClass &&func() {
+  return MyClass{}; // Returning reference to temporary object, UB , it being
+                    // rvalue does not change anything
+}
+
+// How to return result later , ie some operations are needed after
+// perfectReturnFunc calls foo but want to return result of foo
+template <typename Func, typename... Args>
+decltype(auto) call(Func func, Args &&...args) {
+  decltype(auto) ret = func(std::forward<Args>(args)...);
+  // do some operation
+
+  if constexpr (std::is_rvalue_reference_v<decltype(ret)>) {
+    return std::move(ret);
+  } else {
+    return ret;
+  }
+}
+
+std::vector<std::string> create_svec() {
+  return {"Abdu", "Ali", "Alex"}; // Copilot suggested names :)
+}
+
+MyClass ReturnMyClass() { return MyClass{}; }
+
+auto main() -> int {
+  const std::string constStr = "const";
+  std::string nonConstStr = "nonconst";
+  processContainer(constStr);
+  processContainer(nonConstStr);
+  // func("Abdu","Ali"); ;// inputs are different type , const chart[4]/[5]
+  // error ambiguity
+  func("Abdu", "Alex"); // Both are const char[4], with reference types function
+                        // decay to pointer does not happen
+
+  std::vector<std::string> vec;
+  std::string s;
+  //  insert1(vec,s); // Invalid T is deducted as string(1st arg) and string&
+  //  (2nd arg)
+  insert2(vec, s); // Now both string& so it is ok.
+
+  auto &&r1 = MyClass(); // Why do we have auto&&
+  foo(MyClass{});        // This is same with 2 line belove
+  auto &&var1 = MyClass{};
+  foo(std::forward<decltype(var1)>(var1)); // Work same as 2 line above
+
+  // This is useful for case below
+  // T x = function()
+  // foo(x)
+  // But we want this to work same as foo(function())
+  //  Done like this:
+  // auto&& x = function();
+  // foo(std::forward<decltype(x)>(x))
+
+  // Digress to vector<bool>
+
+  std::vector<bool> ivec(4, false);
+  auto x = ivec[2]; // Type of X : reference (aka std::_Bit_reference)
+  for (auto i : ivec) {
+    i = true; // This does the unexpected and values inside ivec really change
+  }
+
+  for (auto i : ivec) {
+    std::cout << i << " ";
+  }
+  std::cout << "\n";
+  ivec.operator[](3).operator=(true);
+  // ivec.operator[](3) returns reference (aka std::_Bit_reference) and we
+  // assign value to it
+  auto iter = ivec.begin();
+  *iter = true; // This will also assign value to reference type
+
+  for (auto x : ivec) {
+    // use x;
+  }
+
+  // this loop is written like this by compiler
+  /*
+    auto&& rng = ivec;
+    auto pos = rng.beging();
+    auto end = rng.end();
+    for(;pos!=end;++pos){
+      auto x = *pos; if defined likefor(auto x:ivec) or
+      auto& x =*pos; if defined likefor(auto& x:ivec)or
+      auto&& x = *pos; if defined likefor(auto&& x:ivec)
+    }
+  */
+
+  int num = 4;
+  decltype(auto) y = num;    // y is int
+  decltype(auto) y2 = (num); // y2 is int&
+  // decltype(auto) &y2 = (num); This does not work, decltype(auto) cant take &
+  // after it.
+
+  auto fn =
+      [](auto &&r1) -> decltype(auto) { // default return type of lambda is auto
+    return foo(std::forward<decltype(r1)>(
+        r1)); // To have same return type , we need trailing return type as
+              // decltype(auto)
+  };
+
+  for (std::string s : create_svec()) { // no problem
+    // ...
+  }
+
+  const auto &r3 = create_svec().at(0); // No lifetime extension
+  // vector ( ret val of create_svec) gets destroyed, boom, this is the reason
+  // why loops below are UB
+
+  // for(char c: create_svec()[0]){ // UB , if uncommented I get seg fault
+  // // for(char c: create_svec().at(0)){ // UB
+  // // for(char c: create_svec().front()){ // UB
+  //   // ...
+  // }
+
+  // With C++23 problem above with UB or for range loops is fixed
+  // Compiler generate it like this :
+  // auto&& temp1 = create_svec();
+  // auto&& temp2 = temp1[0]; // or temp1.at(0) or temp1.front()
+
+  MyClass{} = MyClass{};             // This is valid
+  ReturnMyClass() = ReturnMyClass(); // This is valid, no logical use.
+  // Why this is legal ? operator= is not defined like this
+  // operator=(const MyClass& other)& = default; // This would prevent
+  // assignment to rvalue & specifier.
+
+  testBenchmark();
+  return 0;
+}
---
+++ Lessons/Lesson5_personal_side_project.hpp
@@ -0,0 +1,158 @@
+#pragma once
+#include <cassert>
+#include <chrono>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <type_traits>
+
+template <typename Rep = double, typename Period = std::ratio<1>,
+          typename Callable, typename... Args>
+decltype(auto) benchmark(Callable &&callable, Args &&...args) {
+  const auto start = std::chrono::high_resolution_clock::now();
+  using CallableReturnType = std::invoke_result_t<Callable, Args...>;
+  if constexpr (std::is_same_v<CallableReturnType, void>) {
+    std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
+    const auto finish = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<Rep, Period>(finish - start);
+  } else {
+    decltype(auto) result{std::invoke(std::forward<Callable>(callable),
+                                      std::forward<Args>(args)...)};
+    const auto finish = std::chrono::high_resolution_clock::now();
+    const auto function_runtime{
+        std::chrono::duration<Rep, Period>(finish - start)};
+    return std::pair<std::invoke_result_t<Callable, Args...>,
+                     std::chrono::duration<Rep, Period>>(
+        std::forward<decltype(result)>(result), function_runtime);
+  }
+}
+
+inline auto funcToBenchmark(int x) { // Idiotic function to pass time
+  int64_t val{0};
+  for (int64_t i{0}; i < x; i++) {
+    val += i;
+  }
+  return val;
+}
+
+inline int globalVarToTriggerSideEffects = 0;
+
+class BenchmarkTestClass {
+public:
+  BenchmarkTestClass(int x) : x{x} {}
+  int x;
+  inline static int AccessCounter = 0;
+  int &getX() {
+    ++AccessCounter;
+    ++globalVarToTriggerSideEffects;
+    return x;
+  }
+  BenchmarkTestClass(BenchmarkTestClass &&other) {
+    std::cout << "Move constructor called\n";
+    x = other.x;
+    other.x = 0;
+  }
+  BenchmarkTestClass(const BenchmarkTestClass &other) {
+    std::cout << "Copy constructor called\n";
+    x = other.x;
+  }
+  BenchmarkTestClass &operator=(const BenchmarkTestClass &other) {
+    std::cout << "Copy assignment called\n";
+    x = other.x;
+    return *this;
+  }
+  BenchmarkTestClass &operator=(BenchmarkTestClass &&other) {
+    std::cout << "Move assignment called\n";
+    x = other.x;
+    other.x = 0;
+    return *this;
+  }
+};
+
+inline BenchmarkTestClass &&func1(BenchmarkTestClass &&x) {
+  funcToBenchmark(1000000);
+  x.x = 3;
+  return std::move(x);
+}
+
+inline BenchmarkTestClass func2(BenchmarkTestClass x) {
+  funcToBenchmark(1000000);
+  return x;
+}
+
+void test_function_1() { std::cout << "Time me : " << std::endl; }
+
+int &test_function_2(int x) {
+  static int counter = 0;
+  counter += x;
+  return counter;
+}
+
+class RefTest {
+public:
+  int a;
+  int &GetA() { return a; }
+};
+
+inline void testBenchmark() {
+  BenchmarkTestClass a = 5;
+  a.getX() = 4;
+  auto [resultbenchmark, time] = benchmark(func1, std::move(a));
+  std::cout << resultbenchmark.x << "Time : " << time << "\n";
+  assert(resultbenchmark.x == 3);
+
+  // Above case needs more investigation and improvement
+
+  // Below are simple cases, using auto instead of decltype(auto) works due to
+  // structured binding,std::get and std::pair implementation
+
+  // This works for references even though you would think this would create a
+  // copy Reason is std::get is internally used at structural binding resolution
+  // for pairs and tuple and it has return type&&  .
+  //
+  // Normal return by values is also valid and works as expected
+  std::cout << "Test function 1 Runtime: " << benchmark(test_function_1).count()
+            << std::endl;
+  auto timed_result{benchmark(test_function_2, 5)};
+  decltype(auto) function2_result = timed_result.first;
+  std::cout << "Test function 2 Runtime: " << timed_result.second.count()
+            << std::endl;
+  std::cout << "Return value of funtion 2 " << function2_result << std::endl;
+  function2_result = 7;
+  auto [function2_result2, function2_time2]{benchmark(test_function_2, 5)};
+  std::cout << "Static variable result from reference " << function2_result
+            << " Result of new call " << function2_result2
+            << " should be equal and be 12" << std::endl;
+
+  RefTest ref_test(10);
+  auto [a_ref, member_var_access_time]{benchmark(&RefTest::GetA, &ref_test)};
+  std::cout << "Member variable access time: " << member_var_access_time.count()
+            << std::endl;
+  a_ref = 12;
+  std::cout << "a_ref value: " << a_ref << " a inside ref_test: " << ref_test.a
+            << std::endl;
+  ref_test.a = 9;
+  std::cout << "a_ref value: " << a_ref << " a inside ref_test: " << ref_test.a
+            << std::endl;
+
+  auto [a_ref2, member_var_access_time_ns]{
+      benchmark<int64_t, std::nano>(&RefTest::GetA, &ref_test)};
+  auto [a_ref3, member_var_access_time_micros]{
+      benchmark<double, std::micro>(&RefTest::GetA, &ref_test)};
+  auto [a_ref4, member_var_access_time_ms]{
+      benchmark<double, std::milli>(&RefTest::GetA, &ref_test)};
+  auto [a_ref5,
+        member_var_access_time_s]{benchmark<double>(&RefTest::GetA, &ref_test)};
+  auto [a_ref6, member_var_access_time_decas]{
+      benchmark<double, std::deca>(&RefTest::GetA, &ref_test)};
+  std::cout << "Member variable access time in ns: "
+            << member_var_access_time_ns.count() << std::endl;
+  std::cout << "Member variable access time in microsecond: "
+            << member_var_access_time_micros.count() << std::endl;
+  std::cout << "Member variable access time in millisecond: "
+            << member_var_access_time_ms.count() << std::endl;
+  std::cout << "Member variable access time in second: "
+            << member_var_access_time_s.count() << std::endl;
+  std::cout << "Member variable access time in decasecond: "
+            << member_var_access_time_decas.count() << std::endl;
+}
